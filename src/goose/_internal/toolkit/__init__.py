513249fe49e877174f01a308f24dc72277105749
Lifei Zhou <lifei@squareup.com>
2024-09-04 16:07:09 +1000

    created github folder for github toolkit

--- src/goose/_internal/toolkit/__init__.py
+++ src/goose/_internal/toolkit/__init__.py
@@ -3,7 +3,7 @@ from .repo_context.repo_context import RepoContext  # noqa: F401
 from .summarization.summarize_repo import SummarizeRepo  # noqa
 from .summarization.summarize_project import SummarizeProject  # noqa
 from .summarization.summarize_file import SummarizeFile  # noqa: F401
-from .github import Github  # noqa: F401
+from .github.github import Github  # noqa: F401
 from .screen import Screen  # noqa: F401
 
 

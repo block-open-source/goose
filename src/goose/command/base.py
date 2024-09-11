1d20e15aeeccefe71a89a7da956ac930df827b17
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-09-11 10:19:38 -0700

    chore: docstring improvements

--- src/goose/command/base.py
+++ src/goose/command/base.py
@@ -8,9 +8,20 @@ class Command(ABC):
     """A command that can be executed by the CLI."""
 
     def get_completions(self, query: str) -> List[Completion]:
-        """Get completions for the command."""
+        """
+        Get completions for the command.
+
+        Args:
+            query (str): The current query.
+        """
         return []
 
     def execute(self, query: str, surrounding_context: str) -> Optional[str]:
-        """Execute's the command and replaces it with the output."""
+        """
+        Execute's the command and replaces it with the output.
+
+        Args:
+            query (str): The query to execute.
+            surrounding_context (str): The full user message that the query is a part of.
+        """
         return ""

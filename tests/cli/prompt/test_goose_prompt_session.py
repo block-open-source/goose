3fb42db23aab18839e61d49116c320fd6b30c41d
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-09-11 10:06:16 -0700

    fix: all tests

--- tests/cli/prompt/test_goose_prompt_session.py
+++ tests/cli/prompt/test_goose_prompt_session.py
@@ -1,5 +1,7 @@
+import asyncio
 from unittest.mock import patch
 
+from prompt_toolkit import PromptSession
 import pytest
 from goose.cli.prompt.goose_prompt_session import GoosePromptSession
 from goose.cli.prompt.user_input import PromptAction, UserInput
@@ -24,24 +26,33 @@ def test_get_save_session_name_with_space(mock_prompt_session):
 
     assert goose_prompt_session.get_save_session_name() == "my_session"
 
-    assert user_input == UserInput(PromptAction.CONTINUE, "input_value")
+
+@pytest.mark.asyncio
+def test_get_user_input_to_continue():
+    with patch.object(PromptSession, "prompt", return_value="input_value"):
+        goose_prompt_session = GoosePromptSession()
+
+        user_input = goose_prompt_session.get_user_input()
+
+        assert user_input == UserInput(PromptAction.CONTINUE, "input_value")
 
 
+@pytest.mark.asyncio
 @pytest.mark.parametrize("exit_input", ["exit", ":q"])
 def test_get_user_input_to_exit(exit_input, mock_prompt_session):
-    mock_prompt_session.prompt.return_value = exit_input
-    goose_prompt_session = GoosePromptSession()
+    with patch.object(PromptSession, "prompt", return_value=exit_input):
+        goose_prompt_session = GoosePromptSession()
 
-    user_input = goose_prompt_session.get_user_input()
+        user_input = goose_prompt_session.get_user_input()
 
-    assert user_input == UserInput(PromptAction.EXIT)
+        assert user_input == UserInput(PromptAction.EXIT)
 
 
 @pytest.mark.parametrize("error", [EOFError, KeyboardInterrupt])
 def test_get_user_input_to_exit_when_error_occurs(error, mock_prompt_session):
-    mock_prompt_session.prompt.side_effect = error
-    goose_prompt_session = GoosePromptSession()
+    with patch.object(PromptSession, "prompt", side_effect=error):
+        goose_prompt_session = GoosePromptSession()
 
-    user_input = goose_prompt_session.get_user_input()
+        user_input = goose_prompt_session.get_user_input()
 
-    assert user_input == UserInput(PromptAction.EXIT)
+        assert user_input == UserInput(PromptAction.EXIT)

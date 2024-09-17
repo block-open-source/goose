dde3366bd434055d4b6dac480666f4601e751df6
Luke Alvoeiro <lalvoeiro@squareup.com>
2024-09-17 09:50:17 -0700

    feat: give commands the ability to execute logic (#63)

--- src/goose/cli/prompt/lexer.py
+++ src/goose/cli/prompt/lexer.py
@@ -5,6 +5,11 @@ from prompt_toolkit.document import Document
 from prompt_toolkit.lexers import Lexer
 
 
+# These are lexers for the commands in the prompt. This is how we
+# are extracting the different parts of a command (here, used for styling),
+# but likely will be used to parse the command as well in the future.
+
+
 def completion_for_command(target_string: str) -> re.Pattern[str]:
     escaped_string = re.escape(target_string)
     vals = [f"(?:{escaped_string[:i]}(?=$))" for i in range(len(escaped_string), 0, -1)]
@@ -13,22 +18,21 @@ def completion_for_command(target_string: str) -> re.Pattern[str]:
 
 def command_itself(target_string: str) -> re.Pattern[str]:
     escaped_string = re.escape(target_string)
-    return re.compile(rf"(?<!\S)(\/{escaped_string})")
+    return re.compile(rf"(?<!\S)(\/{escaped_string}:?)")
 
 
 def value_for_command(command_string: str) -> re.Pattern[str]:
-    escaped_string = re.escape(command_string)
-    return re.compile(rf"(?<=(?<!\S)\/{escaped_string})([^\s]*)")
+    escaped_string = re.escape(command_string + ":")
+    return re.compile(rf"(?<=(?<!\S)\/{escaped_string})(?:(?:\"(.*?)(\"|$))|([^\s]*))")
 
 
 class PromptLexer(Lexer):
     def __init__(self, command_names: List[str]) -> None:
         self.patterns = []
         for command_name in command_names:
-            full_command = command_name + ":"
-            self.patterns.append((completion_for_command(full_command), "class:command"))
-            self.patterns.append((value_for_command(full_command), "class:parameter"))
-            self.patterns.append((command_itself(full_command), "class:command"))
+            self.patterns.append((completion_for_command(command_name), "class:command"))
+            self.patterns.append((value_for_command(command_name), "class:parameter"))
+            self.patterns.append((command_itself(command_name), "class:command"))
 
     def lex_document(self, document: Document) -> Callable[[int], list]:
         def get_line_tokens(line_number: int) -> Tuple[str, str]:

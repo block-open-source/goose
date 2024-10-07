029162beb9ba07e3c6da29fe1807770db932d83e
Lily Delalande <ldelalande@squareup.com>
2024-10-07 07:45:23 -0400

    update

--- src/goose/toolkit/test_o1.py
+++ src/goose/toolkit/test_o1.py
@@ -24,12 +24,13 @@ def test_ask_o1():
 
     o1_plan = O1Plan(notifier=LoggingNotifier())
 
-    resp = o1_plan.create_plan_with_o1("I want to make goose into a memgpt model -- give it tools to shuffle things in and out of its context and store and lookup things from external memory. Write a plan to implement this.")
+    #  -- give it tools to shuffle things in and out of its context and store and lookup things from external memory. Write a plan to implement this
+    resp = o1_plan.create_plan_with_o1("I want to make goose into a memgpt model -- give it tools to shuffle things in and out of its context and store and lookup things from external memory.", relevant_files=["memgpt_to_text.txt"])
 
     print("done thinking")
 
     print(resp.content[0].text)
 
-    with open("o1_response.txt", "w") as f:
+    with open("o1_response_2.txt", "w") as f:
         f.write(resp.content[0].text)
 

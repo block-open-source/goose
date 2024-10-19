99605348a6bb531807b331a4cfa059ecc70003a9
Mic Neale <michael.neale@gmail.com>
2024-10-19 11:07:08 +1100

    start of realtime

--- packages/exchange/src/exchange/providers/__init__.py
+++ packages/exchange/src/exchange/providers/__init__.py
@@ -5,6 +5,7 @@ from exchange.providers.anthropic import AnthropicProvider  # noqa
 from exchange.providers.base import Provider, Usage  # noqa
 from exchange.providers.databricks import DatabricksProvider  # noqa
 from exchange.providers.openai import OpenAiProvider  # noqa
+from exchange.providers.openai_realtime import OpenAiRealtimeProvider  # noqa
 from exchange.providers.ollama import OllamaProvider  # noqa
 from exchange.providers.groq import GroqProvider  # noqa
 from exchange.providers.azure import AzureProvider  # noqa
